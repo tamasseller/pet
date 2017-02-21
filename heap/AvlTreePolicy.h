ec40170ce50290888bd90e010f264362d848f56f
Tamás Seller <seller.tamas@gmail.com>
2017-02-21 22:00:01 +0100

    Fixed copyright banners

--- heap/AvlTreePolicy.h
+++ heap/AvlTreePolicy.h
@@ -1,20 +1,21 @@
 /*******************************************************************************
- * Copyright (c) 2017 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     IBM Corporation - initial API and implementation
+ * Copyright (c) 2017 Tamás Seller. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  *******************************************************************************/
-/*
- * AvlTreePolicy.h
- *
- *  Created on: 2016.11.17.
- *      Author: tamas.seller
- */
-
 #ifndef AVLTREEPOLICY_H_
 #define AVLTREEPOLICY_H_
 
