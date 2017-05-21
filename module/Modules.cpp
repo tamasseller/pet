e9562d6368375e13671312f5b53db49577c8eddb
Tamás Seller <seller.tamas@gmail.com>
2017-05-21 15:07:30 +0200

    Inlineify Modules::init

--- module/Modules.cpp
+++ module/Modules.cpp
@@ -4,49 +4,4 @@
  *      Author: tamas.seller
  */
 
-#include "Modules.h"
-
-using namespace pet;
-
-bool Modules::recurse(InitList& initList, ModuleInterface* current)
-{
-    if(current->locked)
-        return false;
-
-    if(current->visited)
-        return true;
-
-    current->locked = true;
-
-    ModuleInterface* const * dependencies;
-    int n = current->requires(dependencies);
-
-    for(int i = 0; i < n; i++) {
-        if(!recurse(initList, dependencies[i]))
-            return false;
-    }
-
-    current->visited = true;
-    initList.addBack(current);
-
-    current->locked = false;
-    return true;
-}
-
-bool Modules::init()
-{
-    InitList initList;
-
-    for(auto it = Registry<ModuleInterface>::iterator(); it.current(); it.step()) {
-        if(!recurse(initList, it.current()))
-            return false;
-    }
-
-    for(auto it = initList.iterator(); it.current(); it.step()) {
-        it.current()->init();
-    }
-
-    return true;
-}
-
 
--- module/Modules.h
+++ module/Modules.h
@@ -15,10 +15,52 @@ namespace pet {
 class Modules {
         typedef LinkedList<ModuleInterface> InitList;
 
-        static bool recurse(InitList& initList, ModuleInterface* current);
+        static inline bool recurse(InitList& initList, ModuleInterface* current);
     public:
-        static bool init();
+        static inline bool init();
 };
+
+
+inline bool Modules::recurse(InitList& initList, ModuleInterface* current)
+{
+    if(current->locked)
+        return false;
+
+    if(current->visited)
+        return true;
+
+    current->locked = true;
+
+    ModuleInterface* const * dependencies;
+    int n = current->requires(dependencies);
+
+    for(int i = 0; i < n; i++) {
+        if(!recurse(initList, dependencies[i]))
+            return false;
+    }
+
+    current->visited = true;
+    initList.addBack(current);
+
+    current->locked = false;
+    return true;
+}
+
+inline bool Modules::init()
+{
+    InitList initList;
+
+    for(auto it = Registry<ModuleInterface>::iterator(); it.current(); it.step()) {
+        if(!recurse(initList, it.current()))
+            return false;
+    }
+
+    for(auto it = initList.iterator(); it.current(); it.step()) {
+        it.current()->init();
+    }
+
+    return true;
+}
 
 }
 
