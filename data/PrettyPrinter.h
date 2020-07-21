fc97cc8e02ab56f08d320e4f045d7e3c2f67d3f1
Tamás Seller <tamas.seller@supercharge.io>
2020-07-21 20:19:02 +0200

    Add gdb xmethod for accessing elements of single and double linked lists

--- data/PrettyPrinter.h
+++ data/PrettyPrinter.h
@@ -3,6 +3,8 @@
 
 __attribute__((section(".debug_gdb_scripts"), used))
 static inline const char __gdb_pretty_printer_data[] = R"foo(gdb.inlined-script
+import gdb.xmethod
+import re
 
 class listIterator:
     def __init__(self, ptr):
@@ -41,6 +43,37 @@ pp = gdb.printing.RegexpCollectionPrettyPrinter("pet")
 pp.add_printer('LinkedList', '^pet::LinkedList.*$', linkedListPrinter)
 pp.add_printer('DoubleList', '^pet::DoubleList.*$', linkedListPrinter)
 gdb.printing.register_pretty_printer(gdb.current_objfile(), pp)
+
+class linkedListIndexerWorker(gdb.xmethod.XMethodWorker):
+    def __init__(self, class_type):
+        self.class_type = class_type
+        self.element_type = class_type.template_argument(0)
+
+    def get_arg_types(self):
+        return gdb.lookup_type('int')
+
+    def get_result_type(self):
+        return self.element_type.poiter()
+
+    def __call__(self, obj, n):
+        it = obj["first"]
+        while n != 0 and it != 0:
+            n = n - 1
+            it = it["next"]
+        return it
+
+class linkedListIndexerMatcher(gdb.xmethod.XMethodMatcher):
+    def __init__(self):
+        gdb.xmethod.XMethodMatcher.__init__(self, 'linkedListIndexerMatcher')
+ 
+    def match(self, class_type, method_name):
+        print(class_type)
+        print(method_name)
+        if re.match('pet::DoubleList<[ \t\n]*[_a-zA-Z][ :_a-zA-Z0-9]*>', class_type.tag) and method_name == 'operator[]':
+            return linkedListIndexerWorker(class_type)
+
+gdb.xmethod.register_xmethod_matcher(None, linkedListIndexerMatcher())
+
 )foo";
 
 #endif
