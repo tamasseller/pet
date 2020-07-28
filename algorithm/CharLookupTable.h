87504d7b577a43393d11172b079c1c09699ae9cf
Tamás Seller <tamas.seller@supercharge.io>
2020-07-28 13:11:15 +0200

    Add compile time generated, table lookup based, fast character trait classification components

---
+++ algorithm/CharLookupTable.h
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2020 Seller Tamás. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *******************************************************************************/
+
+#ifndef CHARLOOKUPTABLE_H_
+#define CHARLOOKUPTABLE_H_
+
+#include "meta/Sequence.h"
+#include "algorithm/Math.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+namespace pet
+{
+
+namespace detail
+{
+	template<char...> struct FindMax
+	{
+		static constexpr char value = 0;
+	};
+
+	template<char c, char... cs> struct FindMax<c, cs...>
+	{
+		static constexpr auto value = pet::max(c, FindMax<cs...>::value);
+	};
+
+	template<char... c> struct IsOneOf
+	{
+		static inline constexpr bool check(...) { return false; }
+	};
+
+	template<char c, char... cs> struct IsOneOf<c, cs...>
+	{
+		static inline constexpr bool check(char d)
+		{
+			return c == d || IsOneOf<cs...>::check(d);
+		}
+	};
+
+	template<class Seq, size_t, char... cs> class MakeWord;
+	template<int... is, size_t base, char... cs> class MakeWord<pet::Sequence<is...>, base, cs...>
+	{
+		using Set = IsOneOf<cs...>;
+		static constexpr bool bits[] = {Set::check(base + is)...};
+
+	public:
+		static constexpr inline uint32_t flatten(int idx = 31)
+		{
+			return (idx >= 0) ? (((bits[idx] ? 1 : 0) << idx) | flatten(idx - 1)) : 0;
+		}
+	};
+
+	template<class Seq, char... cs> class MakeTable;
+	template<int... is, char... cs> class MakeTable<pet::Sequence<is...>, cs...>
+	{
+	public:
+		static inline constexpr uint32_t data[] = {MakeWord<pet::sequence<0, 32>, is * 32, cs...>::flatten()...};
+	};
+}
+
+template<char... cs>
+class CharLookupTable
+{
+	static constexpr auto limit = detail::FindMax<cs...>::value + 1;
+	static constexpr auto nWords = (limit + 31u) / 32u;
+	using Table = detail::MakeTable<pet::sequence<0, nWords>, cs...>;
+
+public:
+	static constexpr inline bool contains(char c)
+	{
+		uint8_t v = c;
+
+		if(v >= limit)
+			return false;
+
+		return Table::data[v >> 5u] & 1u << (v & 31u);
+	}
+};
+
+}
+
+#endif /* CHARLOOKUPTABLE_H_ */
---
+++ algorithm/CharTraits.h
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2020 Seller Tamás. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *******************************************************************************/
+
+#ifndef CHARTRAITS_H_
+#define CHARTRAITS_H_
+
+#include "algorithm/CharLookupTable.h"
+
+namespace pet
+{
+
+struct CharTraits
+{
+	static inline bool isSpace(char c) {
+		return CharLookupTable<' ', '\t', '\r', '\n'>::contains(c);
+	}
+
+	static inline bool isOperator(char c) {
+		return CharLookupTable<'+', '-', '*', '/', '%', '<', '>', '=', '|', '&', '^', '~', '!'>::contains(c);
+	}
+
+	static inline bool isUpper(char c) {
+		return CharLookupTable<'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'>::contains(c);
+	}
+
+	static inline bool isLower(char c) {
+		return CharLookupTable<'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'>::contains(c);
+	}
+
+	static inline bool isAlpha(char c) {
+		return CharLookupTable<'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+							   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'>::contains(c);
+	}
+
+	static inline bool isIdInitial(char c) {
+		return CharLookupTable<'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+							   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+							   '_'>::contains(c);
+	}
+
+	static inline bool isDigit(char c) {
+		return CharLookupTable<'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'>::contains(c);
+	}
+
+	static inline bool isIdInternal(char c) {
+		return CharLookupTable<'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+							   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+							   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+							   '_'>::contains(c);
+	}
+
+	static inline bool isComma(char c) {
+		return c == ',';
+	}
+
+	static inline bool isDot(char c) {
+		return c == '.';
+	}
+
+	static inline bool isDoubleQuote(char c) {
+		return c == '"';
+	}
+
+	static inline bool isSingleQuote(char c) {
+		return c == '\'';
+	}
+
+	static inline bool isOpenParen(char c) {
+		return c == '(';
+	}
+
+	static inline bool isCloseParen(char c) {
+		return c == ')';
+	}
+
+	static inline bool isOpenCurly(char c) {
+		return c == '{';
+	}
+
+	static inline bool isCloseCurly(char c) {
+		return c == '}';
+	}
+
+	static inline bool isOpenBox(char c) {
+		return c == '[';
+	}
+
+	static inline bool isCloseBox(char c) {
+		return c == ']';
+	}
+
+	static inline bool isBackslah(char c) {
+		return c == '\\';
+	}
+};
+
+}
+
+#endif /* CHARTRAITS_H_ */
