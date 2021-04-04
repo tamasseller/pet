5220af6bfb29c3681175aeccb0666324abdf6bcd
Tamás Seller <seller.tamas@gmail.com>
2021-04-04 16:42:58 +0200

    Add option to mod.mk to not include 1test source files in SOURCES

--- mod.mk
+++ mod.mk
@@ -3,8 +3,11 @@ curdir := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 INCLUDE_DIRS := $(INCLUDE_DIRS) $(curdir)/.
 
 SOURCES := $(SOURCES) $(curdir)/data/PrettyPrinter.cpp
+
+ifeq ($(PET_NO_1TEST),)
 SOURCES := $(SOURCES) $(curdir)/1test/TestRunner.cpp
 SOURCES := $(SOURCES) $(curdir)/1test/TraceOutput.cpp
 SOURCES := $(SOURCES) $(curdir)/1test/FailureInjector.cpp
+endif
 
 undefine curdir
